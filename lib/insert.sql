842fce8d65a4a2f1482eb86a35193bd36eaf93b4
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-05-23 18:09:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
 INSERT INTO bears(id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 5, "Male", "Dark Brown", "friendly", true)
+INSERT INTO bears(id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 5, "Male", "Black", "Wild", true)
+INSERT INTO bears(id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 5, "Female", "Orange", "Happy", false)
+INSERT INTO bears(id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 5, "Male", "Brown", "Stern", true)
+INSERT INTO bears(id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 5, "Female", "White", "Friendly", false)
+INSERT INTO bears(id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 5, "Male", "Green", "Angry", true)
+INSERT INTO bears(id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 5, "Female", "Dark Brown", "friendly", false)
+INSERT INTO bears(id, name, age, gender, color, temperament, alive) VALUES (8, "Mr. Chocolate", 5, "Male", "Black", "Angry", true)
