941c12a3075babc6f6d678cc1f161166195bb9b3
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-05-23 18:05:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears(id, name, age, gender, color, temperment, alive) VALUES (1, "Mr. Chocolate", 5, "Male", "Dark Brown", "friendly", true)
+INSERT INTO bears(id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 5, "Male", "Dark Brown", "friendly", true)
